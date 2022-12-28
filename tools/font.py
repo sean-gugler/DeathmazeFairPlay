e5c1c9fb12bf0bd90addb7111114b7f8a7cb9f10
Sean Gugler <sean.gugler@gmail.com>
2022-12-27 18:01:54 -0800

    actually, can fit all font alignments on one HGR screen

--- tools/font.py
+++ tools/font.py
@@ -15,7 +15,6 @@ def usage(argv):
     p.add_argument('input', help="PRG file for font data")
     p.add_argument('output', help="output, PRG file in HGR format")
     p.add_argument('offset', help="offset (hex) where font data is found in input")
-    p.add_argument('-c', '--compact', action='store_true', help="compact image")
     return p.parse_args(argv[1:])
 
 # Byte offsets to scan lines (bitmap rows) in Apple II HGR memory, in vertical order
@@ -32,38 +31,23 @@ def main(argv):
     frame = bytearray(0x2000 - 8)
     font = iter(src[i:])
     fname = args.output
-    if args.compact:
-        draw_compact(frame, font, fname)
-    else:
-        draw_sparse(frame, font, fname)
 
-    return 0
-
-def draw_compact(frame, font, fname):
     for row in range(8):
         for col in range(16):
             for line in range(8):
                 b = next(font)
-                i = screen[row * 8 + line] + col
+                i = screen[row * 8 + line] + col + 4
                 frame[i] = b
                 i += 17
                 frame[i] = b
-
-    with open(fname, 'wb') as out:
-        load_addr = b'\x00\x40'  # lo+hi
-        out.write(load_addr + frame)
-
-def draw_sparse(frame, font, fname):
-    for row in range(8):
-        for col in range(16):
-            for line in range(8):
-                b = next(font)
-                i = screen[(row + 1) * 8 * 2 + line] + (col + 1) * 2
+                i = screen[(row * 2 + 9) * 8 + line] + col * 2 + 5
                 frame[i] = b
 
     with open(fname, 'wb') as out:
         load_addr = b'\x00\x40'  # lo+hi
         out.write(load_addr + frame)
+
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
